e0879b44d8f486bc4584396b99849f123c540bc7
Đạt Nguyễn <43862701+nguyentubadat833@users.noreply.github.com>
2023-04-03 17:02:35 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #pip install --upgrade pip
-./opt/render/project/src/.venv/bin/python3.8 -m pip install --upgrade pip
+venv/bin/python3.8 -m pip install --upgrade pip
 pip install -r requirements_new.txt
